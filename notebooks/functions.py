d20a265cc355abee0c73c8d76fdf37dc1fe31891
RobN <vietlhp@yahoo.de>
2025-07-23 23:54:56 +0200

    Add explore Data nb

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -16,23 +16,6 @@ def map_group_age(age: int):
         return "elder" 
     else: return "NA"
 
-
-
-# def count_backward_steps(group):
-#     step_order = ['start', 'step_1', 'step_2', 'step_3', 'complete']
-#     step_index = {step: idx for idx, step in enumerate(step_order)}
-#     steps = group['process_step'].values
-#     backward_steps = 0
-#     for i in range(1, len(steps)):
-#         current_step_idx = step_index.get(steps[i], float('inf'))
-#         prev_step_idx = step_index.get(steps[i-1], float('inf'))
-#         if current_step_idx < prev_step_idx:
-#             backward_steps += 1
-#     return pd.Series({
-#         'total_steps': len(steps),
-#         'backward_steps': backward_steps
-#     })
-
 def calculate_error_rate(group):
     step_order = ['start', 'step_1', 'step_2', 'step_3', 'complete']
     step_index = {step: idx for idx, step in enumerate(step_order)}
@@ -53,3 +36,11 @@ def calculate_error_rate(group):
         'backward_steps': backward_steps,
         'error_rate': error_rate
     })
+
+def has_all_steps(group: pd.Grouper):
+    steps = {'start', 'step_1', 'step_2', 'step_3', 'confirm'}
+    return steps.issubset(set(group['process_step']))
+
+def has_all_treatment_groups(group: pd.Grouper):
+    steps = {"Test","Control"}
+    return steps.issubset(set(group["treatment_group"]))
